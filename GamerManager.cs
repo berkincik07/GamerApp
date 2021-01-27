73cf97adf1b658381ce14c8ca081c99dbc1e0a07
Root <Root@DESKTOP-89ILC7V>
2021-01-28 00:44:47 +0300

    Game Project First Commit

---
+++ GamerManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    // MicroService
+    class GamerManager : IGamerService
+    {
+        IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer))
+            {
+                Console.WriteLine("Kayıt Oldu!");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama Başarısız. Kayıt Başarısız");
+            }
+            
+             
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Güncellendi!");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Silindi!");
+        }
+    }
+}
